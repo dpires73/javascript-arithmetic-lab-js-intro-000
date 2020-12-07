fb283019004adb2254997f837a2a76d58cec1ea8
David Pires <dpires73@yahoo.com>
2020-12-07 20:38:00 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,39 @@
+number = 10
+function add(){
+  number += 5
+}
+
+function subtract(){
+  number-= 5
+}
+
+function multiply(){
+  number *= 5
+}
+
+function divide(){
+  number /= 5
+}
+
+add()
+console.log(number);
+
+subtract()
+console.log(number);
+
+multiply()
+console.log(number);
+
+divide()
+console.log(number);
+
+
+//a = 5;
+//b = 10;
+c
+function add(5, 10){
+  c = 5+10
+}
+
+add()
+console.log(c);
